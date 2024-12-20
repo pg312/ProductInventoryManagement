2cc362479c14a69a27b93326360b345a9656233c
pg312 <vpadmagade@gmail.com>
2024-12-20 15:55:21 +0530

    Initial commit

---
+++ src/test/java/com/example/ProductsStockManagement/ProductsStockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductsStockManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsStockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
